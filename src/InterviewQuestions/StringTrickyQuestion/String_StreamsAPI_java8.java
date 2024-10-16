252e04a4cf0e6f6e61babf060c32cc115c4ce0ec
riteshPatil <ritesh93patil@outlook.com>
2024-10-16 13:21:03 +0530

    Interview Question Pratice

--- src/InterviewQuestions/StringTrickyQuestion/String_StreamsAPI_java8.java
+++ src/InterviewQuestions/StringTrickyQuestion/String_StreamsAPI_java8.java
@@ -2,6 +2,7 @@ package InterviewQuestions.StringTrickyQuestion;
 
 import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -10,7 +11,7 @@ public class String_StreamsAPI_java8 {
 
     public static void main(String[] args) {
 
-        //First non Repeating Char os a String
+        //First non Repeating Char of a String
        String str = "SWISS";
 
        Character nonRepeatingChar = Arrays.stream(str.split(""))
@@ -24,6 +25,19 @@ public class String_StreamsAPI_java8 {
                .orElse(null);
 
         System.out.println("First Non Repeating Char of String : "+nonRepeatingChar);
+
+        /**
+         *  Reverse each word of the list of strings
+         */
+
+        List<String> inputList = List.of("The quick brown fox jumps over the lazy dog");
+
+        String result= inputList.stream()
+                .map(word->new StringBuilder(word).reverse().toString())
+                .collect(Collectors.joining(" "));
+
+        System.out.println(result);
+
 
 
 
